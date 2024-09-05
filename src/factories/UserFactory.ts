89d87289bf75acc365f0cf14ca3d7972b5bf8ebe
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-09-05 19:33:40 -0300

    refactor(UserFactory): refactor in return type and try catch
    Added return type of Promise<User> and surrounded it with try-catch block

--- src/factories/UserFactory.ts
+++ src/factories/UserFactory.ts
@@ -2,15 +2,19 @@ import { User } from "../models/User";
 import bcrypt from "bcrypt";
 
 export class UserFactory {
-  static async createUser(email: string, password: string) {
-    if (!validateEmail(email)) {
-      console.log("invalid_email");
-      alert("Email is invalid");
-      return;
+  static async createUser(email: string, password: string): Promise<User> {
+    try {
+      if (!validateEmail(email)) {
+        console.log("invalid_email");
+        alert("Email is invalid");
+        throw new Error("Email is invalid");
+      }
+      const hashedPassword = await bcrypt.hash(password, 10);
+      return new User(email, hashedPassword);
+    } catch (error) {
+      console.error("Error creating user:", error);
+      throw error;
     }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    return new User(email,hashedPassword);
-
     function validateEmail(email: string): boolean {
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
       return emailRegex.test(email);
