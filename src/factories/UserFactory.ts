9107b231761169d1afffca958a5c4503a6442356
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-09-07 13:03:49 -0300

    refactor(UserFactory): add new field
    Added field name and validation

--- src/factories/UserFactory.ts
+++ src/factories/UserFactory.ts
@@ -2,15 +2,26 @@ import { User } from "../models/User";
 import bcrypt from "react-native-bcrypt";
 
 export class UserFactory {
-  static async createUser(email: string, password: string): Promise<User> {
+  static async createUser(
+    email: string,
+    password: string,
+    name: string
+  ): Promise<User> {
     try {
       if (!validateEmail(email)) {
         console.log("invalid_email");
         alert("Email is invalid");
         throw new Error("Email is invalid");
       }
+
+      if (!name) {
+        console.log("empty_name");
+        alert("Name is empty");
+        throw new Error("Name is empty");
+      }
+
       const hashedPassword = await bcrypt.hashSync(password, 10);
-      return new User(email, hashedPassword);
+      return new User(email, hashedPassword,name);
     } catch (error) {
       console.error("Error creating user:", error);
       throw error;
